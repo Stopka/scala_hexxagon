92b0cf99d129893b573f66ba542b05f59da98402
Štěpán Škorpil <stepan.s@gmail.com>
2014-04-24 13:50:47 +0200

    Přidána deska bridge

--- app/models/boards/maps/Bridge.scala
+++ app/models/boards/maps/Bridge.scala
@@ -6,7 +6,7 @@ import models.Field
    * Created by stopka on 20.4.14.
    */
 class Bridge extends Map{
-      def getPlayerCount()=1
+      def getPlayerCount()=2
       protected def create()=Array.tabulate[Field](9, 9) {
         (x, y) => {
           if (x < 3 || x > 5||(y==4)||(y==3&&x==5)||(y==5&&x==3)||(x==4&&(y==0||y==8))||(x==3&&(y==0||y==1)||(x==5&&(y==7||y==8)))) {
