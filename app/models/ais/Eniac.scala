151d12095d0ab4ee4a6059090d3c31db2dd21298
Štěpán Škorpil <stepan.s@gmail.com>
2014-04-26 02:46:02 +0200

    základ prohledávání v AI, kosmetické změny

--- app/models/ais/Eniac.scala
+++ app/models/ais/Eniac.scala
@@ -1,13 +1,40 @@
 package models.ais
 
 import models.Game
+import models.Field
+import models.boards.maps.{FilterFieldRange, Map}
 
 /**
  * Created by stopka on 25.4.14.
  */
-object Eniac extends AI{
+object Eniac extends AI with FilterFieldRange{
 
   def findMove(game:Game)={
-    ((0,0),(0,0))
+    val player=game.getPlays()
+    val board=game.getBoard()
+    val myFields=board.getFields().filter(field=>field.getPlayer()==player)
+    val split=getPossibleSplits(board,myFields,splits)
+    val jump=getPossibleSplits(board,myFields,jumps)
+    split.head
+  }
+
+  private def getPossibleSplits(board:Map,fields:Array[Field],method:(Map,(Int,Int))=>Array[Field]):Array[((Int,Int),(Int,Int))]={
+    val fieldFrom=fields.head
+    val from=(fieldFrom.x,fieldFrom.y)
+    val moves=for(field<-method(board,from))
+        yield (from,(field.x,field.y))
+    if(fields.length>1){
+      Array.concat(moves,getPossibleSplits(board,fields.tail,method))
+    }else{
+      moves
+    }
+  }
+
+  private def splits(board:Map,from:(Int,Int))={
+    board.getNearFields(from._1,from._2).filter(field=>field.getPlayer()==(-1)&&field.x!=from._1&&field.y!=from._2)
+  }
+
+  private def jumps(board:Map,from:(Int,Int))={
+    board.getFarFields(from._1,from._2).filterNot(nearFields(from._1,from._2)).filter(field=>field.getPlayer()==(-1))
   }
 }
